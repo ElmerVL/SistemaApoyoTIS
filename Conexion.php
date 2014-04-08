1fdad5112740d60ef9ed1370dff9f799b290bc8a
Elmer <elmer.r.valencia@gmail.com>
2014-04-08 16:05:17 -0400

    esta es la clase conexcion

---
+++ Conexion.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+class Conexion
+{
+	public function getConection()
+
+    {
+        $cadena="host='localhost' port='5432' dbname='Parqueo' user ='postgres' password='VALENCIA'";
+        $con = pg_connect($cadena) or die ('Error en la conexion');
+        return $con;
+    }
+}
+
+
+?>
+
