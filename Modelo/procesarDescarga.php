4aba6a539bcc472cd788062d6c2c0ccab04b656a
tazmancito <draco2287@gmail.com>
2014-04-24 08:23:18 -0400

    subida y bajada terminada

--- Modelo/procesarDescarga.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = file("../Archivos/Doc1.pdf");
-$file2 = implode("", $file);
-header("Content-Type: application/octet-stream");
-header("Content-Disposition: attachment:filename= Doc1.pdf");
-echo $file2;
