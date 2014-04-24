48724f4b1e00d71036e8306173022f82b577b213
Rodrigo <rodrigor.lara64@gmail.com>
2014-04-24 02:04:45 -0400

    no se si reir o llorar!

--- Controlador/ControladorAddActividad.php
+++ Controlador/ControladorAddActividad.php
@@ -1,10 +1,14 @@
 <?php
 require ('../Modelo/ModeloAddActividad.php');
-$autor = $_POST["autor"];
-$titulo = $_POST["titulo"];
-$mensaje = $_POST["mensaje"];
+//$visible_para= $_POST["visible_para"];
+//$req_repuesta= $_POST["requiere_respuesta"];
+$fecha_ini=$_POST["fecha_inicio"];
+$fecha_fin=$_POST["fecha_fin"];
+$titulo=$_POST["txt_titulo"];
+$descripcion=$_POST["ctxt_descripcion"];
 
-agregar($autor, $titulo, $mensaje);
+AddActividad( $fecha_ini,$fecha_fin,$titulo, $descripcion);
+//$visible_para, $req_repuesta,
 
 ?>
 
