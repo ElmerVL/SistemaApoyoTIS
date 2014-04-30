82681f61e8b19b013524977d7c8cb04da31063d4
Rodrigo <rodrigor.lara64@gmail.com>
2014-04-30 19:37:38 -0400

    30/04/2014

---
+++ Controlador/ControladorAddActividadConsultor.php
@@ -0,0 +1,14 @@
+<?php
+require ('../Modelo/ModeloAddActividadConsultor.php');
+$visible_para= $_POST["visible_para"];
+$req_repuesta= $_POST["requiere"];
+$fecha_ini=$_POST["fecha_inicio"];
+$fecha_fin=$_POST["fecha_fin"];
+$hora_ini=$_POST["hora_ini"];
+$hora_fin=$_POST["hora_fin"];
+$titulo=$_POST["txt_titulo"];
+$descripcion=$_POST["ctxt_descripcion"];
+
+AddActividad($visible_para, $req_repuesta,$fecha_ini,$fecha_fin,$hora_ini,$hora_fin,$titulo, $descripcion);
+?>
+
