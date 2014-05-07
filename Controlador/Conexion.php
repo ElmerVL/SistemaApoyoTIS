b7588bd6d33e2a5567cb3c51736d4ee928b26f76
Jessit <Jessit@TazmancitoLT>
2014-05-06 20:12:33 -0400

    sssssss

--- Controlador/Conexion.php
+++ Controlador/Conexion.php
@@ -4,9 +4,9 @@
 class Conexion
 {
 	public function getConection()
-
+                
     {
-        $cadena="host='localhost' port='5432' dbname='SistemaApoyoTIS' user ='postgres' password='tazmancito'";
+        $cadena="host='localhost' port='5432' dbname='pruebas' user ='postgres' password='tazmancito'";
         $con = pg_connect($cadena) or die ('Error en la conexion');
         return $con;
     }
